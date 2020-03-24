1747984ff26e9c5e0f2782171752720fe9a9adb3
George Hou <George Hou>
2020-03-23 20:52:33 -0700

    processed data complete

--- src/analyze_complaints.py
+++ src/analyze_complaints.py
@@ -9,7 +9,18 @@ input_folder = f"{directory}/input/"
 file_name = "complaints.csv"
 file_to_open = input_folder + file_name
 
+processed_data = dict()
 with open(file_to_open) as csv_file:
-    data = csv.reader(csv_file, delimiter=',')
+    data = csv.DictReader(csv_file)
     for row in data:
-        print(row)
+        product = row['Product'].lower()
+        date = row['Date received'][:4]
+        company = row['Company'].lower()
+        if (product, date) in processed_data:
+            if company in processed_data[product, date]:
+                processed_data[product, date][company] += 1
+            else:
+                processed_data[product, date][company] = 1
+        else:
+            processed_data[product, date] = {company: 1}
+print(processed_data)
